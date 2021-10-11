e186ab8a225506697e30e007d5932dd0e046ca4d
psan <pablo.xez@gmail.com>
2021-10-11 11:53:45 +0100

    draft pulling out calendar days based on index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,36 @@
+let currentDisplayMonth = 9
+
+function fillCalendar(month){
+    let date = new Date(2021, month, 1)
+    let calendarDiv = document.getElementById("cal")
+    let dayHTML = document.createElement("div")
+    let totalDays = new Date(2021, month+1, 0).getDate()
+    console.log(`Total days in ${month} is ${totalDays}`)
+    dayHTML.classList.add("day")
+    dayHTML.style.gridColumnStart = date.getDay()
+    dayHTML.innerText = date.getDate()
+    calendarDiv.innerHTML = ""
+    calendarDiv.appendChild(dayHTML)
+    
+
+    for (let i = 2; i <= totalDays; i++){
+        date = new Date(2021, month, i)
+        console.log(i)
+        let dayHTML = document.createElement("div")
+        dayHTML.classList.add("day")
+        dayHTML.innerText = date.getDate()
+        calendarDiv.appendChild(dayHTML)
+    }
+}
+
 document.addEventListener("DOMContentLoaded", start())
 
 function start(){
-    console.log("Start")
+    let today = new Date(2021, 4, 1)
+    console.log(today.getDay())
+    console.log(today.getDate())
+    console.log(today.getMonth()+1)
+    console.log(today.getFullYear())
+
+    fillCalendar(currentDisplayMonth)
 }
